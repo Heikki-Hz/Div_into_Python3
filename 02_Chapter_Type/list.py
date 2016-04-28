6706d293a0734234a4a57d2322c43c3e69617dfa
Heikki <ceivehuang@gmail.com>
2016-04-28 23:56:48 +0800

    list 的一些操作

--- 02_Chapter_Type/list.py
+++ 02_Chapter_Type/list.py
@@ -11,3 +11,15 @@ print(list[2:4])
 print(list[3:-1])
 print(tinylist*2)
 print(tinylist + list)
+
+#与字符串不同的是，list中的元素是可以改变的
+print('change value of list')
+
+list[0] = 9
+print('list[0] = 9.', 'The list is :',list)
+list[2:5] = [2,3,5]
+print('list[2:5] = [].', 'The list is :',list)
+#删除
+list[2:5] = []
+print('list[2:5] = [].', 'The list is :',list)
+
