ebb6b5f1b6812e853d89e00448e9805d0f466792
Heikki <ceivehuang@gmail.com>
2016-04-28 20:33:48 +0800

    type

---
+++ 02_Chapter_Type/list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+list = ['abcd', 222, 2.23, 'Heikki', 21.2]
+tinylist = ['Heikki', 123]
+
+print(list)
+print(list[0])
+print(list[2:])
+print(list[2:4])
+print(list[3:-1])
+print(tinylist*2)
+print(tinylist + list)
---
+++ 02_Chapter_Type/type.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+counter = 100      # int
+miles   = 100.0    # float
+name    = "Heikki" # char string
+
+print(counter)
+print(miles)
+print(name)
+
+
+#多个变量赋值
+a = b = c = 1
+print(a, b, c)
+
+d, e, f = 1, 2, "Heikki"
+print(d, e, f)
+print('--------------------------')
+
+
+#String
+str = 'Heikki'
+
+print(str)
+print(str[0:2])    
+print(str[0:-1])    
+print(str[0])    
+print(str[2:6])    
+print(str[2:])    
+print(str*2)    
+print(str + ' ' + 'is right')    
+print('_________________________________________')
+
+print('Heik\nki')
+print(r'Heik\nki')
